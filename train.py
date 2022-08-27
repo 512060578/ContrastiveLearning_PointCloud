99278ec8aefc0800f3f60e001ef515ed8ce1ba03
512060578 <wkx512060578@gmail.com>
2022-08-27 21:11:02 +0100

    Update the function for dropping points

--- train.py
+++ train.py
@@ -83,11 +83,12 @@ class CustomAugment(object):
         return flipped_data
 
     def drop_point_cloud(batch_data, rate=0.93):
+        dropped_data = np.zeros(batch_data.shape, dtype=np.float32)
         for b in range(batch_data.shape[0]):
             drop_idx = np.where(np.random.random((batch_data.shape[1]))<=rate)[0]
             if len(drop_idx)>0:
-                batch_data[b,drop_idx,:] = batch_data[b,0,:] # set to the first point
-        return batch_data
+                dropped_data[b,drop_idx,:] = batch_data[b,drop_idx,:] # set to the first point
+        return dropped_data
     
     def _random_apply(self, func, x, p):
         return tf.cond(
